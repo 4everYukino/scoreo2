1e178d737e16645a29a1bf4e600c1eed93f68472
BruceW <sfboy4ever@163.com>
2025-07-31 17:09:05 +0800

    Refine HTTP implement

--- httpd/http_handler.h
+++ httpd/http_handler.h
@@ -9,8 +9,20 @@ class HTTP_Handler
 public:
     bool handle_request(const HTTP_Request& req, HTTP_Response& res);
 
-private:
-    virtual bool handle_post(const HTTP_Request& req, HTTP_Response& res);
+    /// RESTful {
+    bool handle_get(const HTTP_Request& req, HTTP_Response& res);
+    bool handle_post(const HTTP_Request& req, HTTP_Response& res);
+    bool handle_put(const HTTP_Request& req, HTTP_Response& res);
+    bool handle_delete(const HTTP_Request& req, HTTP_Response& res);
+    /// }
+
+protected:
+    /// implement {
+    virtual bool handle_get_i(const HTTP_Request& req, HTTP_Response& res);
+    virtual bool handle_post_i(const HTTP_Request& req, HTTP_Response& res);
+    virtual bool handle_put_i(const HTTP_Request& req, HTTP_Response& res);
+    virtual bool handle_delete_i(const HTTP_Request& req, HTTP_Response& res);
+    /// }
 };
 
 #endif
--- scoring/pool_based_room.cpp
+++ scoring/pool_based_room.cpp
@@ -4,6 +4,8 @@
 
 #include <algorithm>
 
+#include <spdlog/spdlog.h>
+
 using namespace std;
 using namespace rtlib;
 
@@ -31,20 +33,17 @@ bool Pool_Based_Room::init(const string& uuid, const Player_ID& first_player_uui
 bool Pool_Based_Room::join(const Player_ID& player_uuid)
 {
     if (profiles_.count(player_uuid)) {
-        // Log
-
+        spdlog::error("The player '{}' is already in the current room.", player_uuid);
         return false;
     }
 
     if (profiles_.size() >= PLAYER_LIMITS) {
-        // Log
-
+        spdlog::error("Failed to join room, player limit of {} reached.", PLAYER_LIMITS);
         return false;
     }
 
     if (!profiles_.emplace(player_uuid, Player_Profile(player_uuid)).second) {
-        // Log
-
+        spdlog::critical("Failed to insert a new KV to `std::unordered_map`, see logs for more details.");
         return false;
     }
 
@@ -56,14 +55,17 @@ bool Pool_Based_Room::apply_action(const Action& act)
     const auto& from = act.from;
     const auto& to = act.to;
     if (from == to) {
-        // Log
-
+        spdlog::error("Failed to apply action, giver '{}' and receiver '{}' must be different.",
+                      from,
+                      to);
         return false;
     }
 
     if (from != POOL_DUMMY_ID && to != POOL_DUMMY_ID) {
-        // Log
-
+        spdlog::error("Either giver '{}' or receiver '{}' must be pool '{}' in pool based room.",
+                      from,
+                      to,
+                      POOL_DUMMY_ID);
         return false;
     }
 
@@ -74,8 +76,7 @@ bool Pool_Based_Room::apply_action(const Action& act)
     }
 
     if (!current_game_.apply_action(act)) {
-        // Log
-
+        spdlog::error("Current game cannot apply action, see logs for more details.");
         return false;
     }
 
@@ -86,10 +87,13 @@ bool Pool_Based_Room::next_game()
 {
     const auto& src = current_game_.get_profiles();
     if (!all_kv(src, [this](const Player_ID& id, const Player_Profile& prof) {
-        return profiles_.count(id);
-    })) {
-        // Log
+        if (!profiles_.count(id)) {
+            spdlog::critical("Unexpected critical error, the '{}' not in this room.", id);
+            return false;
+        }
 
+        return true;
+    })) {
         return false;
     }
 
@@ -110,8 +114,7 @@ void Pool_Based_Room::record_history()
 bool Pool_Based_Room::dissolve()
 {
     if (pool_score() != 0) {
-        // Log
-
+        spdlog::error("Cannot dissolve this pool based room, the pool's score is not 0.");
         return false;
     }
 
@@ -122,8 +125,7 @@ int64_t Pool_Based_Room::pool_score()
 {
     Player_Profile* pool = current_game_.get(POOL_DUMMY_ID, false);
     if (!pool) {
-        // Log
-
+        spdlog::critical("Unexpected critical error, the 'Dummy player' not in current game.");
         return 0;
     }
 
